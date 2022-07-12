07296255d6fb7e3217c794bec98b6f53ce7f0df8
klymova <anastasiia.klymova@nixsolutions.com>
2022-07-12 23:49:56 +0300

    Added data for rendering

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WORDS_GROUP } from './src/data';
+import Deck from './src/Deck';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Deck data={WORDS_GROUP} />
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d9d9f3',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Deck = ({
+    params,
+}) => (
+    <View>
+        <Text>Deck</Text>
+    </View>
+);
+
+export default Deck;
