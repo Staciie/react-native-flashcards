f8db9b0f6784cd0c07d6949586c6223f43bfc1c2
klymova <anastasiia.klymova@nixsolutions.com>
2022-07-14 01:29:13 +0300

    Enabled cards swiping

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { WORDS_GROUP } from './src/data';
 import Deck from './src/Deck';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+const renderCard = (itemData) => {
+  return (
+    <View style={styles.cardContainer}>
+      <Text style={styles.cartText}>{itemData.term}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Deck data={WORDS_GROUP} />
+      <Deck data={WORDS_GROUP} renderCard={renderCard} renderEmptyList={null} onSwipeLeft={null} onSwipeRight={null}/>
     </View>
   );
 }
@@ -18,4 +29,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  cardContainer: {
+    width: SCREEN_WIDTH-40,
+    height: SCREEN_HEIGHT/3,
+    backgroundColor: '#263859',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cartText: {
+    color: '#dadada',
+    fontSize: 20,
+  }
+})
--- src/Deck.js
+++ src/Deck.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Text, View, Animated, PanResponder, TouchableWithoutFeedback, Easing, Dimensions } from 'react-native';
 
-const Deck = ({
-    params,
-}) => (
-    <View>
-        <Text>Deck</Text>
-    </View>
-);
+const LEFT_SWIPE = 'Left';
+const RIGHT_SWIPE = 'Right';
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+
+const Deck = ({data, renderCard, renderEmptyList, onSwipeLeft, onSwipeRight}) => {
+    
+    const position = useRef(new Animated.Value(0)).current;
+    const [currIndex, setCurrIndex] = useState(0);
+
+    const onSwipe = (direction) => {
+        let x = 0;
+        if (direction === LEFT_SWIPE) {
+          x = -SCREEN_WIDTH*2;
+        } else if (direction === RIGHT_SWIPE) {
+          x = SCREEN_WIDTH*2;
+        }
+        Animated.timing(position, {
+            toValue: x,
+            duration: 250,
+            useNativeDriver: true
+        }).start(() => {
+            position.setValue(0); 
+            setCurrIndex(prevIndex => prevIndex +1 );
+        });
+      }
+
+    const panResponder = useRef(
+        PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (_, gesture) => {
+                position.setValue(gesture.dx)
+            },
+            onPanResponderRelease: (_, gesture) => {
+                if (gesture.dx === 0 && gesture.dy === 0) {
+                    console.log('touched')
+                } else if (gesture.dx > 100) {
+                    onSwipe(RIGHT_SWIPE)
+                } else if (gesture.dx < -100) {
+                    onSwipe(LEFT_SWIPE)
+                } else {
+                    Animated.timing(position, {
+                        toValue: 0,
+                        easing: Easing.bounce,
+                        useNativeDriver: true
+                    }).start()
+                }
+            }
+        })
+    ).current;
+
+    return (
+        data.length > currIndex ? 
+            <Animated.View style={{transform: [{translateX: position}]}} {...panResponder.panHandlers}>
+                <View>
+                     {renderCard(data[currIndex])}
+                </View>
+            </Animated.View> : <Text>No more cards</Text>
+)};
 
 export default Deck;
