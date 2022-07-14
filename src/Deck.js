7788859843d4ef2dc5d50c233dd5561fa788f589
Staciie <anastasiia.klymova.01@gmail.com>
2022-07-14 16:47:48 +0300

    Added card flipping

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { Text, View, Animated, PanResponder, TouchableWithoutFeedback, Easing, Dimensions, StyleSheet } from 'react-native';
 
 const LEFT_SWIPE = 'Left';
@@ -8,18 +8,53 @@ const SCREEN_HEIGHT = Dimensions.get('screen').height;
 
 const Deck = ({data, renderCard, renderEmptyList, onSwipeLeft, onSwipeRight}) => {
     const position = useRef(new Animated.Value(0)).current;
-    const rotation = useRef(new Animated.Value(0)).current;
+    const rotation = useRef(new Animated.Value(50)).current;
     const sideOpacity = useRef(new Animated.Value(1)).current;
+    const [side, setSide] = useState(0);
 
-    const side = 0;
     const [currIndex, setCurrIndex] = useState(0);
 
-    const cardStyle = {
+    const cardRotationA = rotation.interpolate({
+        inputRange: [50, 100],
+        outputRange: ["0deg", "180deg"],
+        extrapolate: "clamp"
+      });
+
+      const cardRotationB = rotation.interpolate({
+        inputRange: [50, 100],
+        outputRange: ["-180deg", "0deg"],
+        extrapolate: "clamp"
+      });
+
+    const cardStyles = {
         opacity: sideOpacity,
         transform: [{
             translateX: position
         }]
+    };
+
+    const cardStylesA = {
+        transform: [{
+            rotateY: cardRotationA
+        }
+    ],
+        zIndex: side === 0 ? 1 : 0
     }
+
+    const cardStylesB = {
+        transform: [{
+            rotateY: cardRotationB
+        }
+    ],
+        zIndex: side === 0 ? 0 : 1
+    }
+
+    const resetCardState = () => {
+        position.setValue(0); 
+        setCurrIndex(prevIndex => prevIndex +1);
+        setSide(0);
+        rotation.setValue(50);
+    };
 
     const onSwipe = (direction) => {
         let x = 0;
@@ -40,8 +75,7 @@ const Deck = ({data, renderCard, renderEmptyList, onSwipeLeft, onSwipeRight}) =>
                 useNativeDriver: true
             })
         ]).start(() => {
-            position.setValue(0); 
-            setCurrIndex(prevIndex => prevIndex +1);
+            resetCardState();
             Animated.timing(sideOpacity, {
                 toValue: 1,
                 duration: 100,
@@ -50,15 +84,33 @@ const Deck = ({data, renderCard, renderEmptyList, onSwipeLeft, onSwipeRight}) =>
         });
       }
 
-    const panResponder = useRef(
-        PanResponder.create({
+      function flip() {
+        const value = side === 0 ? 100 : 50;
+        Animated.timing(rotation, {
+            toValue: 75,
+            duration: 100,
+            easing: Easing.in,
+            useNativeDriver: true
+        }).start(() => {
+            setSide(prev => prev === 0 ? 1 : 0);
+            Animated.timing(rotation, {
+                toValue: value,
+                duration: 100,
+                easing: Easing.in,
+                useNativeDriver: true
+            }).start()
+        })
+      }
+
+    const panResponder = useMemo( 
+        () => PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (_, gesture) => {
                 position.setValue(gesture.dx)
             },
             onPanResponderRelease: (_, gesture) => {
                 if (gesture.dx === 0 && gesture.dy === 0) {
-                    console.log('touched')
+                    flip();
                 } else if (gesture.dx > 100) {
                     onSwipe(RIGHT_SWIPE)
                 } else if (gesture.dx < -100) {
@@ -71,14 +123,15 @@ const Deck = ({data, renderCard, renderEmptyList, onSwipeLeft, onSwipeRight}) =>
                     }).start()
                 }
             }
-        })
-    ).current;
+        }), [side]
+    );
+
 
     return (
         data.length > currIndex ? 
-            <Animated.View style={[styles.cardContainer, {...cardStyle}]} {...panResponder.panHandlers}>
-                <Animated.View style={styles.cardContainer}>{renderCard(data[currIndex], "FRONT")}</Animated.View>
-                <Animated.View style={styles.cardContainer}>{renderCard(data[currIndex], "BACK")}</Animated.View>
+            <Animated.View style={[styles.cardContainer, cardStyles]} {...panResponder.panHandlers}>
+                <Animated.View style={[styles.cardContainer, cardStylesA]}>{renderCard(data[currIndex], "FRONT")}</Animated.View>
+                <Animated.View style={[styles.cardContainer, cardStylesB]}>{renderCard(data[currIndex], "BACK")}</Animated.View>
             </Animated.View> : <Text>No more cards</Text>
 )};
 
